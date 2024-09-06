d33d63a22bc38466e9c24a5c83fdcebe37d30aef
Samarjeet Mohanty <samarjeetmohanty123@gmail.com>
2024-09-06 09:39:47 +0530

    Change DayListItem file from component folder to core folder

--- DEVember/App.tsx
+++ DEVember/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { FlatList, StyleSheet, Text, View } from "react-native";
-import DayListItem from "./src/components/DayListItem";
+import DayListItem from "./src/components/core/DayListItem";
 
 const days = [...Array(24)].map((val, index) => index + 1);
 

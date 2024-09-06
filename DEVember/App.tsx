b70804881efdfe2dc5281946c5fa2c30db74549d
Samarjeet Mohanty <samarjeetmohanty123@gmail.com>
2024-09-06 09:37:17 +0530

    Add advent calendar list

---
+++ DEVember/App.tsx
@@ -0,0 +1,36 @@
+import { StatusBar } from "expo-status-bar";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import DayListItem from "./src/components/DayListItem";
+
+const days = [...Array(24)].map((val, index) => index + 1);
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        contentContainerStyle={styles.content}
+        columnWrapperStyle={styles.column}
+        numColumns={2}
+        data={days}
+        renderItem={({ item }) => <DayListItem day={item} />}
+      />
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+
+  content: {
+    gap: 10,
+    padding: 10,
+  },
+  column: {
+    gap: 10,
+  },
+});
---
+++ DEVember/src/components/DayListItem.tsx
@@ -0,0 +1,34 @@
+import { View, Text, StyleSheet } from "react-native";
+
+// * Defining typescript
+type DayListItem = {
+  day: number;
+};
+
+export default function DayListItem({ day }: DayListItem) {
+  return (
+    <View style={styles.box}>
+      <Text style={styles.text}>{day}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  box: {
+    backgroundColor: "#F9EDE3",
+
+    flex: 1,
+    aspectRatio: 1,
+
+    borderWidth: StyleSheet.hairlineWidth, // * smallest border  width
+    borderColor: "#9B4521",
+    borderRadius: 20,
+
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#9B4521",
+    fontSize: 70,
+  },
+});

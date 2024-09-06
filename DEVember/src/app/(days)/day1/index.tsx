a5c9beabaf14753638074229209071e84e72adec
Samarjeet Mohanty <samarjeetmohanty123@gmail.com>
2024-09-06 12:05:44 +0530

    Add Stack navigator

--- DEVember/src/app/(days)/day1/index.tsx
+++ DEVember/src/app/(days)/day1/index.tsx
@@ -1,14 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Stack } from "expo-router";
 
 const DayDetailsScreen = () => {
   return (
     <View>
+      <Stack.Screen options={{ title: "Day 1" }} />
       <Text>DayDetailsScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default DayDetailsScreen
+export default DayDetailsScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
